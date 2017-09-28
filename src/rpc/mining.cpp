14cdc69d005b1bce13bf8f2ff1d6c16592aa314a
Simon Mulser <simonmulser@gmail.com>
2017-09-28 15:49:34 +0200

    add log for parsing

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -138,10 +138,14 @@ UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGen
         if (pblock->nNonce == nInnerLoopCount) {
             continue;
         }
+
+        LogPrintf("Simcoin CreateNewBlock(): hash:%s\n", pblock->GetHash().GetHex());
+
         std::shared_ptr<const CBlock> shared_pblock = std::make_shared<const CBlock>(*pblock);
         if (!ProcessNewBlock(Params(), shared_pblock, true, nullptr))
             throw JSONRPCError(RPC_INTERNAL_ERROR, "ProcessNewBlock, block not accepted");
         ++nHeight;
+
         blockHashes.push_back(pblock->GetHash().GetHex());
 
         //mark script as important because it was used at least for one coinbase output if the script came from the wallet
